a8e01b0c7090578acf0d03407e15b19a7b04bf32
Sarath S <saraths391@gmail.com>
2026-02-26 18:10:40 +0000

    Try fixing this error: `Build Error: Export firebaseConfig doesn't exist

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,10 @@
 // Your web app's Firebase configuration
 // These values are specific to your mealiq-39f52 project.
 export const firebaseConfig = {
-  apiKey: "AIzaSyAkzyldqFvwwpTzMQzWCEKy3wR8qaDVJeA",
-  authDomain: "mealiq-39f52.firebaseapp.com",
-  projectId: "mealiq-39f52",
-  storageBucket: "mealiq-39f52.firebasestorage.app",
-  messagingSenderId: "413224856420",
-  appId: "1:413224856420:web:a5908985ad01353b646871",
-  measurementId: "G-WJYN5X3SM1"
+  apiKey: "AIzaSyBWsuxkcR2uMGXHkbgaS3f0mSY342RMDXE",
+  authDomain: "studio-9575638122-44bb6.firebaseapp.com",
+  projectId: "studio-9575638122-44bb6",
+  storageBucket: "studio-9575638122-44bb6.firebasestorage.app",
+  messagingSenderId: "268822938094",
+  appId: "1:268822938094:web:41ab94d2d9913f4d834eff"
 };
