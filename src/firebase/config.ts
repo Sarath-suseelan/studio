8ab5d80c9716ecf66d6adb1fc4b705bdecc698f1
Sarath S <saraths391@gmail.com>
2026-02-28 18:27:33 +0000

    AIzaSyDixOsgPgMjHbAky8mzbT2uH4sl7xjo5is it's new key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 // Your web app's Firebase configuration
 // These values are specific to your mealiq-39f52 project.
 export const firebaseConfig = {
-  apiKey: "AIzaSyAKnhLxtPYHAqUdympLmtdvd-xbXLvohBs",
+  apiKey: "AIzaSyDixOsgPgMjHbAky8mzbT2uH4sl7xjo5is",
   authDomain: "studio-9575638122-44bb6.firebaseapp.com",
   projectId: "studio-9575638122-44bb6",
   storageBucket: "studio-9575638122-44bb6.firebasestorage.app",
